0d3369dcd4b03af3a9abffd845bc732b1ff3d5e7
Sunghak CHONG <lllshapelll975509@gmail.com>
2026-04-25 18:56:44 +1000

    Add: two-integer-sum-ii - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-8.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int fast = 1;
+        int slow = 0;
+        int len = numbers.length;
+        int[] result = {-1, -1};
+
+        while (slow < len) {
+            if (numbers[fast] + numbers[slow] == target) {
+                result[0] = slow + 1;
+                result[1] = fast + 1;
+                return result;
+            }
+            
+            if (fast >= len - 1) {
+                fast = 0;
+                slow++;
+            } else {
+                fast++;
+            }
+        }
+
+        return result;
+    }   
+}
